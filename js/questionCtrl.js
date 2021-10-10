a0bddbd11fa9c48ca50c6a92e3f2b29304172929
GritArt <75662888+GritArt@users.noreply.github.com>
2021-10-10 22:01:05 +0300

    Delete js directory

--- js/questionCtrl.js
+++
@@ -1,8 +0,0 @@
-app.controller('questionCtrl', function($scope) {
-    $scope.typeOfQuestions = [
-        "Where can I find you?",
-        "If I am young can I use it?",
-        "Should I take my ID?",
-        
-    ];
-});
